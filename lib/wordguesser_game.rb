2a5b928e6891a017370547e79403994e4fd9dc2f
Ramya Mohan <ramsy1195@gmail.com>
2025-09-22 16:12:20 -0400

    Complete WordGuesserGame class with passing specs

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -4,8 +4,12 @@ class WordGuesserGame
 
   # Get a word from remote "random word" service
 
+  attr_accessor :word, :guesses, :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   # You can test it by installing irb via $ gem install irb
@@ -20,4 +24,34 @@ class WordGuesserGame
       return http.post(uri, "").body
     end
   end
+
+  def word_with_guesses
+    @word.chars.map { |char|
+    @guesses.include?(char) ? char : '-'
+    }.join
+  end
+
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.empty? || letter.match?(/[^a-zA-Z]/)
+    letter = letter.downcase
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+    if @word.include?(letter)
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+    
+    true
+  end
+
+  def check_win_or_lose
+    if @word.chars.all? { |char| @guesses.include?(char) }
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
+  end
+
 end
